e3449bceab19f3ad2347b60ab6a0e42ae87709c6
최준영 <cjy4780@naver.com>
2025-10-27 17:16:23 +0900

    refactor: Round.java 삭제로 인한 Round 객체 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RaceController;
-import racingcar.model.Round;
 import racingcar.view.RacingView;
 
 public class Application {
     public static void main(String[] args) {
         RacingView racingView = new RacingView();
-        Round round = new Round();
-        RaceController raceController = new RaceController(racingView, round);
+        RaceController raceController = new RaceController(racingView);
 
         raceController.play();
     }
