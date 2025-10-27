475c0e5917bf649633bb77cb559f7cccc2bc4e36
최준영 <cjy4780@naver.com>
2025-10-27 17:00:31 +0900

    refactor: 일급컬렉션으로 분리

---
+++ src/main/java/racingcar/model/CarsList.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import racingcar.exception.RacingException;
+
+public class CarsList {
+    private final List<Car> carNameList = new ArrayList<>();
+
+    public CarsList(String carsName) {
+        RacingException.validateNotBlank(carsName);
+
+        String[] carsNameArray = carsName.split(",");
+
+        // 자동차 이름 중복일 경우 추가
+        for (String carName : carsNameArray) {
+            carNameList.add(new Car(carName));
+        }
+    }
+
+    public List<Car> getCarsList() {
+        return Collections.unmodifiableList(carNameList);
+    }
+}
